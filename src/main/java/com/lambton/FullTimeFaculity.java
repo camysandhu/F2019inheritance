1245754f533edc0706ec8ef9aca2396fc2b35798
macstudent <fmsharmn@gmail.com>
2019-09-23 20:51:42 -0400

    fulltimefaculity class and others

---
+++ src/main/java/com/lambton/FullTimeFaculity.java
@@ -0,0 +1,31 @@
+package com.lambton;
+
+import java.time.LocalDate;
+
+public class FullTimeFaculity extends Faculity {
+
+    private float bonus;
+
+    public FullTimeFaculity(int id) {
+        super(id);
+    }
+
+    public FullTimeFaculity(int id, String firstName, String lastName, LocalDate birthDate, String gender, String designation, float salary, String specialization, float bonus) {
+        super(id, firstName, lastName, birthDate, gender, designation, salary, specialization);
+        this.bonus = bonus;
+    }
+
+    @Override
+    public void display()
+    {
+        super.display();
+        System.out.println("Faculty Type           : Full Time" );
+        System.out.println("Faculty Bonus          : " + this.bonus);
+        System.out.println("Faculty Net Salary     : " + getNetSalary());
+    }
+
+    @Override
+    public void calculateTotalSalary() {
+        setNetSalary(getBaseSalary() + bonus);
+    }
+}
