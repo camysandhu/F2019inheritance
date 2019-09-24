ed545e79dbce338d7fc65529f05a1d7b1efe36e3
macstudent <fmsharmn@gmail.com>
2019-09-23 20:28:25 -0400

    faculity

--- src/main/java/com/lambton/MainExample.java
+++ src/main/java/com/lambton/MainExample.java
@@ -16,10 +16,33 @@ public class MainExample
                 "MADT",
                 2);
 
-        p1=s1;
+          p1=s1;
+          p1.display();
+
+        Faculity f1= new Faculity(1, "Camy",
+                "Sandhu",
+                LocalDate.now(),
+                "Male",
+                "Software Engineer",
+                (float) 1000.000,
+                "Mobile Software Development") ;
+
+        p1=f1;
         p1.display();
 
-        //Faculity f1= new Faculity(1,"Camy","Sandhu",LocalDate.now(),"Male","Software Engineer",1000,"Mobile Software Development");
+
+        double a = 10000.00;
+         int x = 0b0101010;
+        System.out.println(x);
+
+        int y= 00017;
+        System.out.println(x);
+
+        int z= 0x17ABC;
+        System.out.println(z);
+
+        double b = 1_00_____000.00;
+        System.out.println(b);
 
 
         }
